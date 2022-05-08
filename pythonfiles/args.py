f603a63265306504d947e18a7b31f44c880cd710
LuciLua <luci.lua81@gmail.com>
2022-05-08 13:02:58 -0300

    🐍 DateSystem add, documentation in the code

--- pythonfiles/args.py
+++ pythonfiles/args.py
@@ -1,14 +1,31 @@
 # import requests
 import argparse
-from typing import Optional
+from datetime import datetime
+from requests import options
 # import json
 import rich
 import os
 
-os.system('clear')
-
 # Args
-parser = argparse.ArgumentParser(description="Welcome to lissynt")
+parser = argparse.ArgumentParser(
+    description="Welcome to lissynt",
+    prog='tool',
+    formatter_class=lambda prog: argparse.HelpFormatter(
+        prog,
+        max_help_position=100, 
+      )
+  )
+
+args = [
+        ('-p', '--printh', 'print hello', {}),
+        ('-t', '--test', '[OPTIONAL] test argsArePassed', {}),
+        ('-d', '--date', '[OPTIONAL] show what time is it', {},)
+        ]
+
+for args1, args2, description, options in args:  
+     parser.add_argument(args1, args2, help=description, nargs="?", const="-", **options)
+
+args = parser.parse_args()
 
 # Collors
 class Colors:
@@ -18,33 +35,7 @@ class Colors:
     pinkFosco = "#C059D1"
     red = "#FF241D"
 
-def printarHello(msg):
-    rich.print("Hello,[{0} underline] {1}![/] You are very welcome to my code, I hope that you find what need as I have been writing this code specially for devs as well as you".format(Colors.greenHacker, msg))
-    
-    rich.print("Visit my portfólio: [#356DFA underline]https://luci-lua.tk/")
-    
-def helper():
-    rich.print("[{0}] _          _                 ".format(Colors.greenHacker))
-    rich.print("[{0}]| |__   ___| |_ __   ___ _ __".format(Colors.greenHacker))
-    rich.print("[{0}]| '_ \ / _ \ | '_ \ / _ \ '__|".format(Colors.greenHacker))
-    rich.print("[{0}]| | | |  __/ | |_) |  __/ |   ".format(Colors.greenHacker))
-    rich.print("[{0}]|_| |_|\___|_| .__/ \___|_|   ".format(Colors.greenHacker))
-    rich.print("[{0}]             |_|              ".format(Colors.greenHacker))
-    
-    rich.print("[{0}] You can try pass -h or --help as a param for see the commands avaliable".format(Colors.blueBaby))
-    
-def draw(argPassed, test):
-    
-    if argPassed == True:
-        color = Colors.blueBaby
-    else:
-        color = Colors.red
-    
-    rich.print("[{0}]_____________________________".format(Colors.pinkFosco))
-    rich.print("[{0}]Args passed: [{1}]{2} ".format(Colors.pinkFosco, color, argPassed))
-    rich.print("[{0}]_____________________________".format(Colors.pinkFosco))
-    rich.print("[{0}]Args passed: [{1}]{2} ".format(Colors.pinkFosco, color, test))
-   
+# Errors
 def errors(type, msg):
     class Types:
         warning = "#FAB600"
@@ -58,23 +49,75 @@ def errors(type, msg):
     if type == 3: 
         levelError = Types.hardError
     
-    rich.print("[{0}]- Erro: {1}".format(levelError, msg))
+    rich.print("\n[{0} ]- Erro: {1}\n".format(levelError, msg))
 
+# Options: Functions
+def printarHello(msg):
+    rich.print("\nHello,[{0} underline] {1}![/] You are very welcome to my code, I hope that you find what need as I have been writing this code specially for devs as well as you\n".format(Colors.greenHacker, msg))
+    
+    rich.print("Visit my portfólio: [#356DFA underline]https://luci-lua.tk/ \n")
+    
+def helper():
+    rich.print("[{0}]\n _          _                 ".format(Colors.greenHacker))
+    rich.print("[{0}]| |__   ___| |_ __   ___ _ __".format(Colors.greenHacker))
+    rich.print("[{0}]| '_ \ / _ \ | '_ \ / _ \ '__|".format(Colors.greenHacker))
+    rich.print("[{0}]| | | |  __/ | |_) |  __/ |   ".format(Colors.greenHacker))
+    rich.print("[{0}]|_| |_|\___|_| .__/ \___|_|   ".format(Colors.greenHacker))
+    rich.print("[{0}]             |_|              \n".format(Colors.greenHacker))
+    
+    rich.print("[{0}] You can try pass -i for install all packages too\n".format(Colors.blueBaby))
+    
+def argsArePassed(argPassed, test):
+    
+    if argPassed == True:
+        color = Colors.blueBaby
+    else:
+        color = Colors.red
+    
+    rich.print("[{0}]_____________________________".format(Colors.pinkFosco))
+    rich.print("[{0}]Args passed: [{1}]{2} ".format(Colors.pinkFosco, color, argPassed))
+    rich.print("[{0}]_____________________________".format(Colors.pinkFosco))
+    rich.print("[{0}]Args passed: [{1}]{2} ".format(Colors.pinkFosco, color, test))
+   
+def showTheDate():
 
-parser.add_argument('-d', '--debug', help="[OPTIONAL] debug")
-parser.add_argument('-t', '--test', help="[OPTIONAL] test draw", nargs="?", const='nothing')
+    day = datetime.now().day
+    month = datetime.now().month
+    year = datetime.now().year
+    
+    hour = datetime.now().hour
+    min = datetime.now().minute
+    sec = datetime.now().second
+    
+    today = "{0}/{1}/{2}".format(day, month, year)
+    
+    time = "{0}:{1}:{2}".format(hour, min, sec)
+    
+    dateFormat = "\nDate of today: {0} \nWhat time is it: {1}\n".format(today, time)
+    
+    rich.print("[{0}] {1}".format(Colors.blueBaby, dateFormat))
 
-args = parser.parse_args()
-
-if args.debug or args.test:
-    if args.debug:
-        printarHello(args.debug)
-    # Verify if receive a arg
-    elif args.test == 'nothing': 
-        draw(False, False)
+# Conditional structures
+# Case cli have some sufix 
+if args.printh or args.test or args.date:
+    # printh conditionals
+    if args.printh == "-":
+        errors(1, "not arg")
+    elif args.printh:
+        printarHello(args.printh)
+    # Test conditionals
+    if args.test == '-': 
+        argsArePassed(False, False)
     elif args.test:
-        draw(True, args.test)
-        # if not receive a arg
+        argsArePassed(True, args.test)
+    # Date conditionals
+    if args.date == '-': 
+        showTheDate()
+    elif args.date:
+        errors(2, "Please, don't put nothing in this area. Just leave it empty" )
+# Case cli not have sufixes
 else:
-    errors(1, "None arg passed" )
+    errors(1, "None option passed" )
     helper()
+    os.system("cli -h")
+    print("\n")
